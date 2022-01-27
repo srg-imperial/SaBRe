2b2b09920932f72e28b793995fe67992ce3ea8b6
Anastasios Andronidis <anastasios.andronidis@gmail.com>
2022-01-27 21:23:48 +0000

    Increase size of a static array

--- loader/maps.c
+++ loader/maps.c
@@ -33,7 +33,7 @@
 #include "macros.h"
 
 #define MAX_BUF_SIZE PATH_MAX + 1024
-#define MAX_INITIAL_MAPPINGS 41
+#define MAX_INITIAL_MAPPINGS 1024
 
 static long initial_mappings[MAX_INITIAL_MAPPINGS];
 static int initial_mapping_cnt = 0;
