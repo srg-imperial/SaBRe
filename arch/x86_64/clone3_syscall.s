c5b4138011a2d04f3b6fc78cb3ea5d9b4799abc8
Anastasios Andronidis <anastasios.andronidis@gmail.com>
2022-03-03 20:43:11 +0000

    Support clone3

---
+++ arch/x86_64/clone3_syscall.s
@@ -0,0 +1,52 @@
+/*  Copyright © 2019 Software Reliability Group, Imperial College London
+ *
+ *  This file is part of SaBRe.
+ *
+ *  SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+  .file "clone3_syscall.s"
+  .text
+  .globl clone3_syscall
+  .type clone3_syscall, @function
+
+#                                    - userland args: https://elixir.bootlin.com/glibc/glibc-2.35/source/sysdeps/unix/sysv/linux/x86_64/clone3.S#L25
+# long clone3 (long arg1,     # %rdi - struct clone_args *cl_args
+#              long arg1,     # %rsi - size_t size
+#              long arg2,     # %rdx - int (*func)(void *arg)
+#              not_used,      # - %rcx - is clobbered by syscall so we don't use it.
+#              long arg4,     # %r8  - void *arg
+#              void *ret_addr # %r9
+#             );
+
+clone3_syscall:
+  pushq %rbp
+  movq %rsp, %rbp
+
+  # Call clone3
+  movq $435, %rax
+  syscall
+
+  # Both child and parent return here
+  testq  %rax, %rax
+  jnz    1f
+
+  # Child
+  subq $0x80, %rsp # We need the -0x80 as we are jumping back to our trampoline that adds 0x80.
+
+  # From: https://elixir.bootlin.com/glibc/glibc-2.35/source/sysdeps/unix/sysv/linux/x86_64/clone3.S#L52
+  # glibc's clone3 expects %rdx to point to the user provide fn and it's arguments in %r8.
+  # clone3 is significanlty different than clone where arguments are actually passed in the
+  # child's stack. For more look here: https://elixir.bootlin.com/glibc/glibc-2.35/source/sysdeps/unix/sysv/linux/x86_64/clone.S#L98
+  # It's safe to keep %rdx and %r8 unsaved as they are not clobbered from the kernel. For
+  # more look here: https://gitlab.com/x86-psABIs/x86-64-ABI/-/blob/master/x86-64-ABI/kernel.tex#L35-37
+
+  jmp *%r9 # ret_addr jumps back to our trampoline.
+
+1:
+  # Parent
+  popq %rbp
+  ret
+
+  .size clone3_syscall, .-clone3_syscall
+  .section .note.GNU-stack,"",@progbits
--- plugins/sbr-id/identity.c
+++ plugins/sbr-id/identity.c
@@ -17,6 +17,7 @@
 #define _GNU_SOURCE
 #endif
 #include <assert.h>
+#include <linux/sched.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/syscall.h>
@@ -33,6 +34,14 @@ long handle_syscall(long sc_no, long arg1, long arg2, long arg3, long arg4,
     void *ret_addr = get_syscall_return_address(wrapper_sp);
     return clone_syscall(arg1, (void *)arg2, (void *)arg3, (void *)arg4, arg5,
                          ret_addr);
+  } else if (sc_no == SYS_clone3) { // clone3
+    struct clone_args *cl_args = (struct clone_args *)arg1;
+    if (cl_args->stack == 0) {
+      return real_syscall(sc_no, arg1, arg2, arg3, arg4, arg5, arg6);
+    }
+
+    void *ret_addr = get_syscall_return_address(wrapper_sp);
+    return clone3_syscall(arg1, arg2, arg3, 0, arg5, ret_addr);
   } else if (sc_no == SYS_execve) {
     char **old_argv = (char **)arg2; // Just make our life easier.
 
