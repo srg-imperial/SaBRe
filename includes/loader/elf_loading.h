05816ee066a7284bee8afd0e73eeb44455b254b4
Iurii Zamiatin <notiurii@gmail.com>
2022-07-25 14:09:41 +0100

    Mimic GDB external debug info search procedure

--- includes/loader/elf_loading.h
+++ includes/loader/elf_loading.h
@@ -15,6 +15,10 @@
 
 int elfld_getehdr(int, ElfW(Ehdr) *);
 GElf_Sym find_elf_symbol(const char *, const char *, bool *);
+bool read_elf_note(const char *path, Elf64_Word note_type, const char *owner,
+                   void *notebuf, size_t *notesz);
+bool read_elf_section(const char *path, const char *section_name, void *scbuf,
+                      size_t *scsz);
 ElfW(Addr) addr_of_elf_symbol(const char *, const char *, bool *);
 ElfW(Addr) elfld_load_elf(int fd, ElfW(Ehdr) const *ehdr, size_t pagesize,
                           ElfW(Addr) * out_phdr, ElfW(Addr) * out_phnum,
